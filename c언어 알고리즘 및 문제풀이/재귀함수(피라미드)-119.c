c5a6eac55777d279eee6b453c92e70529b3f40ee
SANGHOON KIM <sanghoon273@gmail.com>
2024-07-07 17:50:17 +0900

    Add files via upload

---
+++ c언어 알고리즘 및 문제풀이/재귀함수(피라미드)-119.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int sub(int n) {
+	if (n == 0) return 0;
+	if (n == 1) return 1;
+	return(sub(n - 1) + sub(n - 2));
+}
+int main() {
+	int a = 0;
+	a = sub(4);
+	printf("%d", a);
+	return 0;
+}
+
+//3 Ãâ·Â
+
